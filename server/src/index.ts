301aedb7a2f31f69a56d60dac0471c1827dc7f70
Merul Dhiman <merul@Meruls-MacBook-Pro.local>
2021-11-30 14:35:02 +0530

    connected route

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,13 @@
 import express, { Request, Response } from "express";
 import { PORT, CONN_STRING } from "./config";
 import { connectToDB } from "./utils/mongo";
+import appRoutes from "./routes/app.routes";
 
 connectToDB(CONN_STRING);
 
 const app = express();
+
+app.use("/api/app", appRoutes);
 
 app.listen(PORT, () => {
   console.log(`--> Server running on port ${PORT}`);
